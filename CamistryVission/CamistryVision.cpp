b40fadc4ca6c793b689a54386382a4ac3283c7ad
David <davidstulemeijer@gmail.com>
2022-05-13 15:29:41 +0200

    added entrypoint

---
+++ CamistryVission/CamistryVision.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <opencv2/aruco.hpp>
+
+#include "tigl.h"
+#include <GLFW/glfw3.h>
+
+
+
+double lastUpdateTime;
+void update();
+void draw();
+void init();
+
+int main()
+{
+	std::cout << "start camistry vision" << std::endl;
+
+	if (!glfwInit())
+		throw "Could not initialize glwf";
+
+	auto window = glfwCreateWindow(1000, 800, "CamistryVision", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		throw "Could not initialize glwf";
+	}
+	glfwMakeContextCurrent(window);
+
+	tigl::init();
+
+	init();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		update();
+		draw();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+
+	return 0;
+}
+
+
+void init()
+{
+	lastUpdateTime = glfwGetTime();
+}
+
+void update()
+{
+	double timeNow = glfwGetTime();
+	float deltaTime = timeNow - lastUpdateTime;
+	lastUpdateTime = timeNow;
+
+	std::cout << deltaTime << std::endl;
+}
+
+void draw()
+{
+
+}
