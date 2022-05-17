8bae0101f773f95f65f8eec031fff24167732240
Jesse <jm.krijgsman@student.avans.nl>
2022-05-17 10:10:24 +0200

    Created ModelHandler

--- CamistryVission/Handlers/ModelHandler.cpp
+++ CamistryVission/Handlers/ModelHandler.cpp
@@ -1,9 +1,12 @@
-#include "SceneHandler.h"
+#include "ModelHandler.h"
 
 namespace camvis {
 	namespace handlers {
 
+		void ModelHandler::loadMaterialFile(const std::string& filename, const std::string& directory)
+		{
 
+		}
 
 	}
 }
